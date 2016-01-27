aebfbb5c5c08895996c9260f6de335131aabc72e
Albert Manya <albert.manya@schibsted.com>
2016-01-27 07:52:44 +0100

    Unit tests for getInstance

--- addsg_test.go
+++ addsg_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"testing"
+
 	"github.com/amanya/addsg/mocks"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 type AddSGSuite struct {
@@ -43,5 +46,41 @@ func (s *AddSGSuite) TestGetSecurityGroupDidExists() {
 
 	sg, err := helper.getSecurityGroup("asdf", "zxcv")
 	s.Equal(sg, "asdf")
+	s.Equal(err, nil)
+}
+
+func (s *AddSGSuite) TestGetInstanceNotFound() {
+	client := new(mocks.EC2er)
+	client.On("DescribeInstances", mock.AnythingOfType("*ec2.DescribeInstancesInput")).Return(
+		&ec2.DescribeInstancesOutput{
+			Reservations: []*ec2.Reservation{},
+		}, nil)
+
+	helper := &EC2Helper{client}
+
+	i, err := helper.getInstance("1.1.1.1")
+	assert.Nil(s.T(), i)
+	s.Equal(err, fmt.Errorf("instance not found"))
+}
+
+func (s *AddSGSuite) TestGetInstanceFound() {
+	client := new(mocks.EC2er)
+	client.On("DescribeInstances", mock.AnythingOfType("*ec2.DescribeInstancesInput")).Return(
+		&ec2.DescribeInstancesOutput{
+			Reservations: []*ec2.Reservation{
+				&ec2.Reservation{
+					Instances: []*ec2.Instance{
+						&ec2.Instance{
+							InstanceId: aws.String("i-asdfasdf"),
+						},
+					},
+				},
+			},
+		}, nil)
+
+	helper := &EC2Helper{client}
+
+	i, err := helper.getInstance("1.1.1.1")
+	s.Equal(i.InstanceId, aws.String("i-asdfasdf"))
 	s.Equal(err, nil)
 }
